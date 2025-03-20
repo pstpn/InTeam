7a902fa0e035484cd398be9c62510fbf849148c5
mshsvnv <mshsvnv@mail.ru>
2025-03-20 22:58:32 +0300

    [*]: started user's feedbacks view

--- src/frontend/racket-shop/src/views/User/FeedbacksView.vue
+++ src/frontend/racket-shop/src/views/User/FeedbacksView.vue
@@ -1,6 +1,42 @@
 <template>
     <div class="container-page">
         <h1 class="font-container-header">Мои отзывы</h1>
+
+        <div class="font-grid-2">
+            <p>Вы не оставили еще ни один отзыв</p>
+        </div>
+
+        <div class="grid-order-column">
+            <div class="grid-card-feedback">
+                <div class="grid-feedback">
+                    <p class="font-form-header">
+                        Ракетка 1
+                    </p>
+                    
+                    <div class="form-in-row-racket">
+                        <div class="grid-photo-ball">
+                            <p class="font-grid-2">
+                                Дата
+                            </p>
+                            <img src="../../assets/ball.png">
+                            <img src="../../assets/ball.png">
+                            <img src="../../assets/ball.png">
+                            <img src="../../assets/ball.png">
+                        </div>
+                    </div>
+                </div>
+                <p class="font-grid-2">
+                    класс ракетка!
+                </p>
+                <div class="form-in-row-right">
+                    <button 
+                        class="submit-button-orange">
+                        Удалить
+                    </button>
+                </div>
+                <hr class="line">
+            </div>
+        </div>
     </div>
 </template>
 
--- src/frontend/racket-shop/src/views/User/OrdersView.vue
+++ src/frontend/racket-shop/src/views/User/OrdersView.vue
@@ -1,10 +1,153 @@
 <template>
     <div class="container-page">
         <h1 class="font-container-header">Мои заказы</h1>
+
+        <div v-if="orders.length > 0">
+            <div class="grid-order-column" v-for="(order, index) in orders" :key="index">
+                <div class="grid-card-name">
+                    <div class="form-in-row">
+                        <button 
+                            :class="getStatusButtonClass(order.status)"
+                            @click="handleOrderAction(order)">
+                            {{ getStatusButtonText(order.status) }}
+                        </button>
+                        <p class="font-grid-2-bold">
+                            Цена: {{ order.total_price }} ₽
+                        </p>
+                    </div>
+                    <div class="form-in-row">
+                        <p class="font-grid-2">
+                            Товары
+                        </p>
+                        <p class="font-grid-2-bold">
+                            {{ order.items.length }} шт.
+                        </p>
+                    </div>
+                    <div class="form-in-row">
+                        <p class="font-grid-2">
+                            Получатель
+                        </p>
+                        <p class="font-grid-2-bold">
+                            {{ order.recipient_name }}
+                        </p>
+                    </div>
+                    <div class="form-in-row">
+                        <p class="font-grid-2">
+                            Адрес доставки
+                        </p>
+                        <p class="font-grid-2-bold">
+                            {{ order.delivery_address }}
+                        </p>
+                    </div>
+                    <div class="form-in-row">
+                        <p class="font-grid-2">
+                            Дата доставки
+                        </p>
+                        <p class="font-grid-2-bold">
+                            {{ formatDate(order.delivery_date) }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div v-else class="font-grid-2">
+            <p>У вас пока нет заказов</p>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import config from "../../../config.js";
+
+export default {
+    name: 'AppOrders',
+    data() {
+        return {
+            orders: []
+        };
+    },
+    methods: {
+        async fetchOrders() {
+            try {
+                const token = localStorage.getItem('token');
+
+                if (!token) {
+                    this.$router.push(this.config.API.auth.login);
+                    return;
+                }
+
+                const response = await axios.get(`${config.BACKEND_URL}${config.API.user.orders}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+
+                console.log(response)
+                if (response.data) {
+                    this.orders = response.data.orders;
+                }
+            } catch (error) {
+                console.error('Ошибка при получении заказов:', error);
+            }
+        },
+
+        getStatusButtonClass(status) {
+            switch (status) {
+                case 'InProgress':
+                    return 'status-button-inprogress';
+                case 'Canceled':
+                    return 'status-button-canceled';
+                case 'Done':
+                    return 'status-button-done';
+                default:
+                    return 'status-button-default';
+            }
+        },
+        getStatusButtonText(status) {
+            switch (status) {
+                case 'InProgress':
+                    return 'В обработке';
+                case 'Canceled':
+                    return 'Отменен';
+                case 'Done':
+                    return 'Завершен';
+                default:
+                    return 'Неизвестно';
+            }
+        },
+
+        async handleOrderAction(order) {
+            if (order.status === 'InProgress') {
+                try {
+                    const token = localStorage.getItem('token');
+                    const response = await axios.post(
+                        `${config.BACKEND_URL}${config.API.user.cancelOrder}/${order.id}`,
+                        {},
+                        {
+                            headers: { Authorization: `Bearer ${token}` }
+                        }
+                    );
+
+                    if (response.data) {
+                        this.fetchOrders(); 
+                    }
+                } catch (error) {
+                    console.error('Ошибка при отмене заказа:', error);
+                }
+            }
+        },
+
+        formatDate(dateString) {
+            const date = new Date(dateString);
+            return date.toLocaleDateString('ru-RU');
+        }
+    },
+    mounted() {
+        this.fetchOrders(); 
+    }
+};
 </script>
 
 <style>
